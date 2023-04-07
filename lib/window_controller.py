9b0e0b67505c9da0ce91a1a47762b17d676a440e
Aarys <33986208+aaryswastaken@users.noreply.github.com>
2023-04-07 20:22:51 +0200

    raciste.

--- lib/window_controller.py
+++ lib/window_controller.py
@@ -6,7 +6,7 @@
 """
 
 from __future__ import absolute_import
-from tkinter import Tk, Canvas ,StringVar, Label
+from tkinter import Canvas, StringVar, Label
 from random import randint
 from PIL import Image, ImageTk
 
