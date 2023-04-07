24e0b0183286356e611fd547d1afef5c9ccd7482
Aarys <33986208+aaryswastaken@users.noreply.github.com>
2023-04-07 20:14:47 +0200

    ...

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,8 @@
+"""
+    This module wrapper allows us to use the actual 
+    libraries without too much pain
+"""
+
 import lib.event_pool_controller
 import lib.generator
 import lib.window_controller
